04890563587af4a5733aa005bbb5fa5c56949f01
kyw <rla4062002@naver.com>
2020-03-17 22:04:39 +0900

    delete file

--- users/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models  # 이건 안 씀
-
-
-class User(AbstractUser):  # models.Model을 상속
-
-    bio = models.TextField(default="")
