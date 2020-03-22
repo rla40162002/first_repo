c0838b3e253221f286496d94d0a80a2cb8b00f94
kyw <rla4062002@naver.com>
2020-03-23 04:17:44 +0900

    overriding save method

--- rooms/admin.py
+++ rooms/admin.py
@@ -28,7 +28,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Time", {"fields": ("check_in", "check_out", "instant_book")},),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")},),
