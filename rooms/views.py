70c75312b5d072ccd7d65619b9c8fb4466bca0a2
kyw <rla4062002@naver.com>
2020-03-27 18:45:44 +0900

    Paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,15 @@
-from math import ceil
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
-    # all_rooms.html 과 all_rooms 메소드, all_rooms 변수가 같은 이름일 필요 없다.
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))  # page list중에서 요청된 페이지를 가져옴
+        return render(request, "rooms/home.html", context={"page": rooms},)
+        # all_rooms.html 과 all_rooms 메소드, all_rooms 변수가 같은 이름일 필요 없다.
+    except EmptyPage:  # 어떤 예외를 핸들링 할 건지
+        return redirect("/")
