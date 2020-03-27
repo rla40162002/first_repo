c3cef27710c86454c1a46786dac31cafce60db5d
kyw <rla4062002@naver.com>
2020-03-27 19:35:11 +0900

    Class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))  # page list중에서 요청된 페이지를 가져옴
-        return render(request, "rooms/home.html", context={"page": rooms},)
-        # all_rooms.html 과 all_rooms 메소드, all_rooms 변수가 같은 이름일 필요 없다.
-    except EmptyPage:  # 어떤 예외를 핸들링 할 건지
-        return redirect("/")
+class HomeView(ListView):
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
