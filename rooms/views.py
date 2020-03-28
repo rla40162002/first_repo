d77d1eac1c7ca52fc9cc6ea6d3224b1e7633f1ed
kyw <rla4062002@naver.com>
2020-03-29 01:42:57 +0900

    Search form start

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -24,3 +26,15 @@ class RoomDetail(DetailView):
 
     model = models.Room
     pk_url_kwarg = "pk"  # pk 대신 다른 거로 대체 가능
+
+
+def search(request):
+    print(request.GET)
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
