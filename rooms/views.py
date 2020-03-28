1c65c41cf4490f4a07c6182ffacc9b6867d2f5bc
kyw <rla4062002@naver.com>
2020-03-28 21:51:26 +0900

    Room detail

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -21,5 +22,8 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    print(pk)
-    return render(request, "rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
+    return render(request, "rooms/detail.html", {"room": room})
