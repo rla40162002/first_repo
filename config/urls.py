d50dceeee314460001d4b1c1efef90afacdc9786
kyw <rla4062002@naver.com>
2020-03-28 20:48:40 +0900

    URL arguments

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
     path("admin/", admin.site.urls),
 ]  # 이부분 변경하면 접속할때의 주소가 바뀜.
 
