74cb077ceb99368cbb9cb90cc62073abff77515d
kyw <rla4062002@naver.com>
2020-03-30 04:40:05 +0900

    Login FormView version

--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
 from django.views import View
+from django.views.generic import FormView
 from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+    """  def get(self, request):
         form = forms.LoginForm(initial={"email": "test@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
@@ -20,7 +35,7 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form}) """
 
 
 def log_out(request):
