f7203a57a289e35e34a336d1d90553e1963faac6
kyw <rla4062002@naver.com>
2020-04-09 03:32:52 +0900

    Background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   theme: {
-    extend: {},
-  },
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+      } // spacing
+    }, // extend
+    borderRadius: {
+      xl: "1.5rem",
+    }
+  }, // theme
   variants: {},
   plugins: [],
-}
+} // module.exports
