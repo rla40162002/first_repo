83360a7f6202bfd00e7bfd929370cb0fef54d0b7
kyw <rla4062002@naver.com>
2020-04-13 21:50:51 +0900

    Room Detail front

--- users/views.py
+++ users/views.py
@@ -267,8 +267,8 @@ class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView
 
 
 class UpdatePasswordView(
-    mixins.EmailLoginOnlyView,
     mixins.LoggedInOnlyView,
+    mixins.EmailLoginOnlyView,
     SuccessMessageMixin,
     PasswordChangeView,
 ):
