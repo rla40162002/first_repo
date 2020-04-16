c28eb313e25f77ee8c8b2449ec7aef91d7581b80
kyw <rla4062002@naver.com>
2020-04-17 04:28:08 +0900

    Session Check

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect, reverse
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.decorators import login_required
 from smtplib import SMTPException, SMTPResponseException
 from django.core.files.base import ContentFile
 from django.contrib import messages
@@ -286,3 +287,12 @@ class UpdatePasswordView(
 
     def get_success_url(self):
         return self.request.user.get_absolute_url()
+
+
+@login_required
+def switch_hosting(request):
+    try:
+        del request.session["is_hosting"]
+    except KeyError:
+        request.session["is_hosting"] = True
+    return redirect(reverse("core:home"))
