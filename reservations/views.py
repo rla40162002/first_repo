f2c56b6f72afc711ceee5002244afc70014e446d
kyw <rla4062002@naver.com>
2020-04-29 03:30:11 +0900

    Model Managers

--- reservations/views.py
+++ reservations/views.py
@@ -28,5 +28,7 @@ def create(request, room, year, month, day):
 
 
 class ReservationDetailView(View):
-    def get(self):
-        pass
+    def get(self, pk):
+        reservation = models.Reservation.objects.get_or_none(pk=pk)
+        if not reservation:  # reservation이 없을 때
+            return redirect(reverse("core:home"))
