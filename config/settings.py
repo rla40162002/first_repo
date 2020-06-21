4386b658b41f7316b4dc5e549489d099ca4428bc
kyw <rla4062002@naver.com>
2020-06-21 14:04:57 +0900

    Fix ebcli

--- config/settings.py
+++ config/settings.py
@@ -93,25 +93,32 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
 
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "PORT": "5432",
-        }
-    }
+}
+# if DEBUG:
+
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "HOST": os.environ.get("RDS_HOST"),
+#             "NAME": os.environ.get("RDS_NAME"),
+#             "USER": os.environ.get("RDS_USER"),
+#             "PASSWORD": os.environ.get("RDS_PASSWORD"),
+#             "PORT": "5432",
+#         }
+#     }
 
 
 # Password validation
